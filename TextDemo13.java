84206ebb1c1763a80c3e67c502a693c417561e3b
史强 <74546606+sqissuper@users.noreply.github.com>
2021-08-25 17:57:55 +0800

    Add files via upload

---
+++ TextDemo13.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * ClassName:TextDemo13
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Author:HP
+ * @date:2021/8/23 19:33
+ */
+public class TextDemo13 {
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        while (in.hasNextInt()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
+//            int n = in.nextInt();
+//            int[] arr1 = new int[n];
+//            int[] arr2 = new int[n];
+//            int[] arr3 = new int[n];
+//            for(int i = 0; i < n; i++) {
+//                arr1[i] = in.nextInt();
+//            }
+//            for(int i = 0; i < n; i++) {
+//                arr2[i] = in.nextInt();
+//            }
+//            for(int i = 0; i < n; i++) {
+//                arr3[i] = in.nextInt();
+//            }
+//            Arrays.sort(arr1);
+//            Arrays.sort(arr2);
+//            Arrays.sort(arr3);
+//            System.out.println(func(arr1,arr2,arr3,n));
+//        }
+        int a;
+        a = 6;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+
+    }
+//    public static int func(int[] arr1,int[] arr2,int[] arr3,int n) {
+//        int min = Integer.MAX_VALUE;
+//        int sum = 0;
+//        for(int i = 0; i < n; i++) {
+//            int tmp = 0;
+//            for(int j = 0; j < n; j++) {
+//                for (int k = 0; k < n; k++) {
+//                    tmp = Math.min(Math.min(arr1[i],arr2[j]),arr3[k]);
+//                }
+//            }
+//            sum += tmp;
+//        }
+//        return sum;
+//    }
+
+}
---
+++ TextDemo14.java
@@ -0,0 +1,10 @@
+/**
+ * ClassName:TextDemo14
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Author:HP
+ * @date:2021/8/25 16:44
+ */
+public class TextDemo14 {
+}
