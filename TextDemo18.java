0c74c44bd8dbecfb08d81722a3d98b4584114b5b
史强 <74546606+sqissuper@users.noreply.github.com>
2021-08-31 23:17:34 +0800

    Add files via upload

---
+++ TextDemo18.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+/**
+ * ClassName:TextDemo18
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Author:HP
+ * @date:2021/8/31 19:20
+ */
+public class TextDemo18 {
+//    public static long totalRun (int days) {
+//        // write code here
+//        if(days < 2) return 100;
+//        int[] dp = new int[days];
+//        dp[0] = 100;
+//        int sum = 100;
+//        for(int i = 1; i < days; i++) {
+//            if(i % 7 == 0) {
+//                dp[i] = dp[i - 7] + 100;
+//            } else {
+//                dp[i] = dp[i - 1] + 100;
+//            }
+//            sum += dp[i];
+//        }
+//        return sum;
+//    }
+
+//    public static int closestValue (int[] array, int target) {
+//        // write code here
+//        if(array.length == 0) return -1;
+//        int max = -Integer.MAX_VALUE;
+//        Arrays.sort(array);
+//        for(int i = 0; i < array.length; i++) {
+//            for(int j = i + 1; j < array.length; j++) {
+//                int tmp = 0;
+//                if(array[i] + array[j] <= target) {
+//                    tmp = array[i] + array[j];
+//                }
+//                max = Math.max(max,tmp);
+//            }
+//        }
+//        return max == 0 ? -1 : max;
+//    }
+    public static void main(String[] args) {
+//        int[] arr = {-1,-3,-5,-9,-7};
+//        int n = 6;
+//        System.out.println(closestValue(arr,n));
+    }
+}
